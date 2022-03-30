304f342d8b421dcfcff008568fbae9bd18b57e38
nmoody9899 <nmoody9899@ufl.edu>
2022-03-29 18:57:53 -0700

    Added responsive links to site pages and social media accounts to the site navigator

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,28 +3,55 @@ import './Navbar.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import GitHubWhite from '../assets/icons/github_white.png';
+import GitHubBlue from '../assets/icons/github_blue.png';
+import LinkedInWhite from '../assets/icons/linkedin_white.png';
+import LinkedInBlue from '../assets/icons/linkedin_blue.png';
+
 const Navbar = () => {
     return (
         <div className='navigation'>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/about'>About</Link>
-                </li>
-                <li>
-                    <Link to='/experience'>Experience</Link>
-                </li>
-                <li>
-                    <Link to='/projects'>Projects</Link>
-                </li>
-                <li>
-                    <Link to='/contact'>Contact</Link>
-                </li>
-            </ul>
+            <div>
+                <div class='icon'>
+                    <a href='https://github.com/nmoody9899'>
+                        <img src={GitHubWhite} /> 
+                    </a>
+                    <a class='color-swap' href='https://github.com/nmoody9899'>
+                        <img src={GitHubBlue} /> 
+                    </a>
+                </div>
+                <div class='icon'>
+                    <a href='https://www.linkedin.com/in/nicholasmoody1/'>
+                        <img src={LinkedInWhite} /> 
+                    </a>
+                    <a class='color-swap' href='https://www.linkedin.com/in/nicholasmoody1/'>
+                        <img src={LinkedInBlue} /> 
+                    </a>
+                </div>
+                <ul>
+                    <li>
+                        <Link to='/' className={`links`}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/about' className={`links`}>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/experience' className={`links`}>Experience</Link>
+                    </li>
+                    <li>
+                        <Link to='/projects' className={`links`}>Projects</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact' className={`links`}>Contact</Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
 
 export default Navbar;
+
+//                 <a href='https://www.linkedin.com/in/nicholasmoody1/'>
+// <img src={LinkedInWhite} />
+// </a>
