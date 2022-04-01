38e5e2934fc68e17a4bf33ee9914c7ee04a72f5e
nmoody9899 <nmoody9899@ufl.edu>
2022-03-31 22:03:50 -0700

    Modified welcome image on Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,15 @@
 import './Home.css';
 
 import React from 'react';
-import Image from '../assets/images/home_image.png';
+import Image from '../assets/images/welcome_image.png';
 
 const Home = () =>{
   return (
-    <div class='welcome'>
+    <div>
       <div>
         <img src={Image} class='welcome-image' alt='Landmannalaugar in Iceland'/>
       </div>
-      <div class='message'>
+      <div class='welcome'>
         <p>👋🏻 Hello! My name is <span>Nicholas Moody</span>.</p>
         <p>📍 I am a Data Engineer and Computer Science graduate student from <span>Jacksonville, Florida</span>, currently residing in <span>Seattle, Washington</span>.</p>
         <p>🏥 I currently develop data pipelines and business intelligence solutions for <span>Johns Hopkins Medicine</span> and study at the <span>Georgia Institue of Technology</span>.</p>
